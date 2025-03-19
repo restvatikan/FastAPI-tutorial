453710ededdddda70610b83f3b85d5e368ad2ec1
Jungbin Song <happysongjb@gmail.com>
2025-03-19 19:39:11 +0900

    main의 4번째 줄에 주석 추가

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI
-#최종 merge 합의?
+# 4번째 줄 주석(main branch)
 # str을 상속하면 ModelName의 멤버를 str처럼 사용 가능(.value 생략 가능)
 # Enum을 상속하면 ModelName에 존재하지 않는 멤버 접근 시 에러 출력
 class ModelName(str, Enum):
