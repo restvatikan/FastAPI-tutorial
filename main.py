5c79d54b7196d03cdc1c48d6cae82af5e5691b9d
Jungbin Song <happysongjb@gmail.com>
2025-03-20 13:21:10 +0900

    250320-132013 업데이트

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
-#250320 092124
+# final update: 250320-132013
 # str을 상속하면 ModelName의 멤버를 str처럼 사용 가능(.value 생략 가능)
 # Enum을 상속하면 ModelName에 존재하지 않는 멤버 접근 시 에러 출력
 class ModelName(str, Enum):
